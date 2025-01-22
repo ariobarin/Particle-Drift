71de3c790a82b90bf6ddf0dcf9cad9025c8e834b
Kevin Dang <kevin.dang.0007@gmail.com>
2025-01-21 20:10:45 -0500

    Update Stepper32.cpp

--- Hardware Programming/main32/src/Stepper32.cpp
+++ Hardware Programming/main32/src/Stepper32.cpp
@@ -1,40 +1,98 @@
-/* Stepper32.h
+/* Stepper32.cpp
 
 Kevin Dang
 
-Header file for Stepper32 class
-*/
+Controls stepper, turns stepper motor by angle or steps, keeps track of steps
+*/ 
 
 
-#include <Arduino.h>
-
-#ifndef Stepper32_H
-#define Stepper32_H
-
-class Stepper32{
-
-    public:
-
-        Stepper32(int dirPin, int stepPin, int stepsPerRevolution);
-        void turnAngle(int angle);
-        void turnSteps(int steps);
-        void setDelayPerStep(int microseconds);
-        int getSteps();
-        double getAngle();
-        void setSpeed(int rpm);
-        //public methods for turning a certain angle, turning a certain number of steps, setting delay per step, getting steps, getting angle, and setting speed
-    private:
-        
-        int dirPin;
-        int stepPin;
-        int stepsPerRevolution;
-        float anglePerStep;
-        int delayPerStep; //microseconds to wait betweens steps to make it run
-        // int delay; //milliseconds delay to make it move smoothly and at a certain speed
-        int currSteps;
-
-};
+#include "Stepper32.h"
 
 
+Stepper32::Stepper32(int dirPin, int stepPin, int stepsPerRevolution){
+	this->dirPin = dirPin; //dirPin controls direction (high for clockwise, low for counterclockwise)
+	this->stepPin = stepPin; //stepPin controls the step
+	this->stepsPerRevolution = stepsPerRevolution;
+	this->anglePerStep = 360.0 / stepsPerRevolution;
+	this->delayPerStep = 1000;
+	this->currSteps = 0;
+	//initialize variables
+	pinMode(dirPin, OUTPUT);
+	pinMode(stepPin, OUTPUT);
 
-#endif
+}
+
+void Stepper32::turnAngle(int angle){
+	
+	int numSteps = round(abs(angle)/anglePerStep);
+	delay(1);
+
+	//find nearest number of steps to turn
+	if (angle > 0){ //if angle is positive, turn on dirPin
+		digitalWrite(dirPin, HIGH);
+		currSteps += numSteps;
+	}
+	else{
+		digitalWrite(dirPin, LOW); //if angle is negative, turn counterclockwise
+		currSteps -= numSteps;
+	}//update currSteps
+
+	currSteps = currSteps % stepsPerRevolution;
+
+	for(int x = 0; x < numSteps; x++)
+	{//turn the motor
+		digitalWrite(stepPin, HIGH);
+		delayMicroseconds(delayPerStep);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(delayPerStep);
+	}
+
+}
+
+void Stepper32::turnSteps(int steps){
+
+	int numSteps = abs(steps);
+	
+	if (steps > 0){
+		digitalWrite(dirPin, HIGH);
+		currSteps += numSteps;
+		
+	}
+	else{
+		digitalWrite(dirPin, LOW);
+		currSteps -= numSteps;
+	}
+	// delay(1);
+	
+	currSteps = currSteps % stepsPerRevolution; //keep value within 0 and stepsPerRevolution
+
+	//same logic as turnAngle
+	for(int x = 0; x < numSteps; x++){
+
+		digitalWrite(stepPin, HIGH);
+		delayMicroseconds(delayPerStep);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(delayPerStep);
+		
+	}
+}
+
+
+
+int Stepper32::getSteps(){
+	return currSteps;
+
+}
+
+double Stepper32::getAngle(){
+	return currSteps*anglePerStep;
+} //getter methods
+
+void Stepper32::setDelayPerStep(int microseconds){
+	delayPerStep = microseconds; //set delay per step
+}
+
+void Stepper32::setSpeed(int rpm) {
+    delayPerStep = 60L * 1000L * 1000L / (rpm * stepsPerRevolution);
+} //set speed in rpm
+
